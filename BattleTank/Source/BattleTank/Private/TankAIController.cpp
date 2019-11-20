bb197e7e2580979a6d11ae9f23fb15f41ac8f28f
sivaniadze <saba.ivaniadze@gmail.com>
2019-11-20 14:53:18 +0400

    TankAIController finding FirstPlayerController Pawn working.

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -2,6 +2,7 @@
 
 
 #include "TankAIController.h"
+#include "Engine.h"
 
 void ATankAIController::BeginPlay()
 {
@@ -16,9 +17,24 @@ void ATankAIController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("AIController possessing: %s"), *ControlledTank->GetName())
 	}
+
+	auto PlayerControlledTank = GetPlayerTank();
+	if (!PlayerControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s says: Player Controller not found!"), *ControlledTank->GetName())
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s has found %s."), *ControlledTank->GetName(), *PlayerControlledTank->GetName())
+	}
 }
 
 ATank* ATankAIController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
 }
