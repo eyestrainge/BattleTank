a16e03d3156ff6982a22d09918793bbae1ba2c9a
sivaniadze <saba.ivaniadze@gmail.com>
2019-12-18 17:18:38 +0400

    Elevation with 8000 LaunchSpeed works 1st time

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -7,4 +7,9 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 {
 	//auto Time = GetWorld()->GetTimeSeconds();
 	//UE_LOG(LogTemp, Warning, TEXT("%f: Barrel->Elevate() called at speed %f"), Time, RelativeSpeed)
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -2, 2);
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
