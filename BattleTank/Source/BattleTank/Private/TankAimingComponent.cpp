e73c52a4248024c6a907c5af557b18c424429e8e
sivaniadze <saba.ivaniadze@gmail.com>
2019-12-19 14:49:10 +0400

    Barrel Turret combo works. (upper Aim conniptions)

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -11,7 +11,7 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true; // TODO Should this really tick?
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
@@ -73,4 +73,5 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 
 	Barrel->Elevate(DeltaRotator.Pitch);
 	//Barrel->Elevate(4);
+	Turret->Rotate(DeltaRotator.Yaw);
 }
