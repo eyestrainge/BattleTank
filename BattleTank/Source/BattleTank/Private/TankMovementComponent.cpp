d70337ae9a51935cc5819b39f41f92c0cee6e909
sivaniadze <saba.ivaniadze@gmail.com>
2019-12-24 18:51:41 +0400

    TankMovementComponent from inherited to BlueprintSpawnable

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,9 +1,19 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+#include "TankTrack.h"
 #include "TankMovementComponent.h"
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw)
+
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
