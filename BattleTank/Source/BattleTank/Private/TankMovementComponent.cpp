7d6040248ccb826dd42fbdb6915779485206618a
sivaniadze <saba.ivaniadze@gmail.com>
2019-12-25 16:46:23 +0400

    IntendMoveForward now takes ForwardThrow DotProduct

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -11,9 +11,14 @@ void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* 
 
 void UTankMovementComponent::RequestDirectMove(const FVector& MoveVelocity, bool bForceMaxSpeed)
 {
-	auto TankName = GetOwner()->GetName();
-	auto MoveVelocityString = MoveVelocity.ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s vectoring to %s"), *TankName, *MoveVelocityString)
+	auto TankForward = GetOwner()->GetActorForwardVector().GetSafeNormal();
+	auto AIForwardIntention = MoveVelocity.GetSafeNormal();
+
+	auto ForwardThrow = FVector::DotProduct(TankForward, AIForwardIntention);
+
+	IntendMoveForward(ForwardThrow);
+
+	UE_LOG(LogTemp, Warning, TEXT("Called IntendMoveForward(float) with %f"), ForwardThrow)
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
