a7498382b81eecd421b0a5660453ef89ad37ea90
sivaniadze <saba.ivaniadze@gmail.com>
2019-12-25 15:05:17 +0400

    WASD and NavMeshBoundsVolume added. AI aiming and Cast UPrimitiveComponent remain issue.

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -5,7 +5,6 @@
 
 void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
@@ -13,7 +12,15 @@ void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw)
-
+	if (!LeftTrack || !RightTrack) { return; }
 	LeftTrack->SetThrottle(Throw);
 	RightTrack->SetThrottle(Throw);
+}
+
+void UTankMovementComponent::IntendMoveRight(float Throw)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Intend move right throw: %f"), Throw)
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(-Throw);
 }
--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -1,17 +1,14 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "TankTrack.h"
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	//auto Time = GetWorld()->GetTimeSeconds();
-	//auto Name = GetName();
-	//UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle)
-
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
+
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+
 	if (TankRoot)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("TankRoot found"))
@@ -19,6 +16,6 @@ void UTankTrack::SetThrottle(float Throttle)
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("TankRoot not found"))
+		//UE_LOG(LogTemp, Warning, TEXT("TankRoot not found"))
 	}
 }
