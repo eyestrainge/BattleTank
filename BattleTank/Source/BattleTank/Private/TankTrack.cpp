0f1e2969b9713c64544962454900b19a95c41542
sivaniadze <saba.ivaniadze@gmail.com>
2020-01-16 18:29:32 +0400

    Sideways friction. Cast from root component still broken (TankRoot)

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -2,20 +2,33 @@
 
 #include "TankTrack.h"
 
+UTankTrack::UTankTrack()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	//UE_LOG(LogTemp, Warning, TEXT("Track ticking"))
+	auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+	auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+	auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
+	if (TankRoot)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankRoot found @ TickComponent"))
+		auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Two tracks
+		TankRoot->AddForce(CorrectionForce);
+	}
+}
+
 void UTankTrack::SetThrottle(float Throttle)
 {
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
-
 	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
-
 	if (TankRoot)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("TankRoot found"))
 		TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
-	}
-	else
-	{
-		//UE_LOG(LogTemp, Warning, TEXT("TankRoot not found"))
 	}
 }
