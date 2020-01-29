2703ff877caf2ba27bed830685d32051ac86009a
sivaniadze <saba.ivaniadze@gmail.com>
2020-01-29 16:46:26 +0400

    Sandbox ready. (1st person camera included)

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -103,7 +103,7 @@ bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVec
 		HitResult,
 		StartLocation,
 		EndLocation,
-		ECollisionChannel::ECC_Visibility)
+		ECollisionChannel::ECC_Camera)
 		)
 	{
 		HitLocation = HitResult.Location;
